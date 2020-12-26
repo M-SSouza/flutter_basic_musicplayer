41d8b0fb3d4e564d8f0f5359190523f2d26945d5
M-SSouza <61476990+M-SSouza@users.noreply.github.com>
2020-12-26 20:31:36 -0300

    migrationStart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_basic_musicplayer/permissions.dart';
-import 'package:flutter_basic_musicplayer/screens/home_screen.dart';
+import 'package:flutter_basic_musicplayer/app/app_module.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
-import 'utilities.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await checkpermissions();
-  await getsongs();
-
-  print('passou');
-
-  runApp(
-    MaterialApp(
-      home: HomeScreen(),
-    ),
-  );
-}
+void main() => runApp(ModularApp(module: AppModule()));
